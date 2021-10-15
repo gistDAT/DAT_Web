5cd9786502363f8aafa57c218d71f37d16e3a67e
qqqaa999 <qqqaa999@naver.com>
2021-10-15 13:01:38 +0900

    --1

--- homeapp/views.py
+++ homeapp/views.py
@@ -6,6 +6,7 @@ import time
 # Create your views here.
 from homeapp.models import places,kywords
 import sys
+from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 def home_view(request):
 
@@ -52,6 +53,7 @@ def home_view(request):
 
             for content in contents:
                 content_list = content_list + content.text  # content_list 라는 값에 + 하면서 점점 누적
+        driver.quit()
         new_keyword = kywords()
         new_keyword.test_set = content_list
         new_keyword.save()
@@ -61,7 +63,7 @@ def home_view(request):
         f_name = 'C://Users//Park JuHwan//PycharmProjects//DAT//blog2.txt'  # 연관분석시 필요한 txt
         f = open(f_name, 'a', encoding='UTF-8')
         sys.stdout = f
-        print(content_list)
+
         sys.stdout = orig_stdout
         f.close
 
@@ -88,9 +90,9 @@ def home_view(request):
             text = tagger.normalize(text)
 
             tokens = tagger.nouns(text)
-            print(tokens)
+
             tokens = [item for item in tokens if item not in word_list]
-            print(tokens)
+
             tokens = [token for token in tokens if len(token) > 1]  # 한 글자인 단어는 제외
             # 불용어 삭제
             f = open('homeapp/한국어불용어100.txt', 'r', encoding='UTF-8')  # 전처리를 위한 불용어 사전
@@ -107,29 +109,28 @@ def home_view(request):
 
         search_list = search.split()
         search_list.append(search)
-        if __name__ == '__main__':
-            twit = Okt()
-            word = keyword_extractor(twit, content, search_list)
+        # if __name__ == '__main__':
+        twit = Okt()
+
+        word = keyword_extractor(twit, content, search_list)
 
         # 1. word2Vec
         from gensim.models.word2vec import Word2Vec
         from konlpy.tag import Okt
         okt = Okt()
-        print(word)
+
         f = open('blog2.txt', 'r', encoding='UTF-8')
         lines = f.readlines()
         dataset = []
         for i in range(len(lines)):
             dataset.append(okt.nouns(lines[i]))
         dataset = [[y for y in x if not len(y) == 1] for x in dataset]
-        print(dataset)
+
         con_dataset = []
         for element in dataset:
             con_dataset += element
 
-        print(con_dataset)
         word_list = [W for W in word if W in con_dataset]
-        print(word_list)
         # import pandas as pd
         model = Word2Vec(dataset, sg=1, window=5, min_count=1)
         model.init_sims(replace=True)
@@ -149,37 +150,33 @@ def home_view(request):
                             real_Keyword.append(w)
         real_Keyword = set(real_Keyword)
         print(real_Keyword)  ## 찐 키워드 검색할 단어
-        # output 크롤링
+        # output
+
+        from googleapiclient.discovery import build
+        from googleapiclient.errors import HttpError
         real_Keyword = list(real_Keyword)
-        keyword1 = real_Keyword[0]
-        driver.get("https://www.youtube.com/")
+        Q = real_Keyword[0] + ',' + real_Keyword[1] + ',' + real_Keyword[2] + ',' + 'playlist'
+        DEVELOPER_KEY = "AIzaSyD4joaYpY6VGQZ1-klJel3pk5ppeAQ8K9w"
+        YOUTUBE_API_SERVICE_NAME = "youtube"
+        YOUTUBE_API_VERSION = "v3"
+        youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
+        search_response = youtube.search().list(
+            q=Q,
+            part="snippet",
 
-        search = driver.find_element_by_xpath('//input[@id="search"]')
+            maxResults=30,
 
-        search.send_keys(real_Keyword[0])
-        time.sleep(1)
+        ).execute()
 
-        search.send_keys(',')
-        time.sleep(1)
-
-        search.send_keys(real_Keyword[1])
-        time.sleep(1)
-
-        search.send_keys(',')
-        time.sleep(1)
-
-        search.send_keys(real_Keyword[2])
-        time.sleep(1)
-        search.send_keys(',')
-        time.sleep(1)
-
-        search.send_keys('음악')
-        time.sleep(1)
-
-        search.send_keys(Keys.ENTER)
-        title = driver.find_element_by_xpath('//input[@id="thumbnail"]')
-        link = title.get_attribute("href")
-        out_put = driver.get(link)
-        return render(request,'subapp/sub_view.html', context={'output':out_put},)
+        ID_list = []
+        print(search_response['items'][0]['id'])
+        for search_result in search_response.get("items", []):
+            ID_list.append(search_result["id"]["videoId"])
+        print(ID_list[0])
+        path = "homeapp/chromedriver.exe"
+        driver = webdriver.Chrome(path)
+        url = 'https://www.youtube.com/watch?v=' + ID_list[0]
+        driver.get(url)
+        return render(request,'homeapp/home_view.html', context={},)
     else:
-        return render(request, 'homeapp/home_view.html', context={'text': '장소 입력 후 잠시만 기다려주세요!'})
+        return render(request,'homeapp/home_view.html', context={'text': '장소 입력 후 잠시만 기다려주세요!'})
